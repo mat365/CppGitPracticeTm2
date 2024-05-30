d0107be04ddc736ca38fa726e58675f0ddec4c86
mat365 <150889120+mat365@users.noreply.github.com>
2024-05-30 16:58:01 +0900

    Implement functions

--- FindFibonacciGcd.cpp
+++ FindFibonacciGcd.cpp
@@ -4,13 +4,34 @@
 /* ‚±‚±‚ÉFindFibonacciGcd.h‚Å’è‹`‚µ‚½ŠÖ”‚ğÀ‘•‚·‚é@*/
 
 uint32_t gcd(uint32_t a, uint32_t b) {
-	return 0;
+
+	if(b == 0)
+		return a;
+	else
+		return gcd(b,a % b);
 }
 
 uint32_t fibonacci(uint32_t n) {
-	return 0;
+
+	if(n <= 1) {
+		return n;
+	}
+
+	return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-uint32_t findFibonacciGcd(uint32_t a, uint32_t b) {
-	return 0;
+uint32_t findFibonacciGcd(uint32_t n, uint32_t m) {
+
+	uint32_t gcdResult = gcd(fibonacci(n),fibonacci(m));
+
+	if(gcdResult == 1) {
+        return 1;
+    }
+
+	uint32_t i = 0;
+	while(fibonacci(i) != gcdResult) {
+		i++;
+	}
+
+	return i;
 }
